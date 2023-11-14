619a837a37abc39679d2b5b71aa6eea11a9e7564
edgar2020 <evmelendrez@gmail.com>
2023-11-13 23:26:47 -0800

    file restructuring

--- keoghs_website/src/index.js
+++ keoghs_website/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './css/index.css';
+// import App from './App';
+import Home from './homePage';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>
 );
 

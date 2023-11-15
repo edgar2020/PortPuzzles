5a4470410238d7c3cd3d868d4610b95726e9c2ff
edgar2020 <evmelendrez@gmail.com>
2023-11-15 00:51:30 -0800

    header changes

--- keoghs_website/src/components/header.js
+++ keoghs_website/src/components/header.js
@@ -1,15 +1,26 @@
 import React,{ Component } from 'react';
 import './header.css';
+import keoghLogo from '../other/placeholder-logo.png'
   
 
 class SiteHeader extends Component
 {
   render () 
   {
-    return <div className='siteHeader'>
-      <h1>Header</h1>
+    return (  
+      <div className='siteHeader'>
+        {/* left */}
+        <div className='keoghLogoContainer'>
+          <img src={keoghLogo} className='headerLogo' alt='Keoghs Logo'/>
+        </div>
 
-    </div>
+        {/* middle */}
+        <h1 className='headerTitle'>Keogh's Port</h1>
+
+        {/* right */}
+        
+      </div>
+    )
   }
 }
 export default SiteHeader;

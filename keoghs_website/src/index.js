00453dae245d92090d589bf8bcb89034d09ee951
edgar2020 <evmelendrez@gmail.com>
2023-11-15 00:12:42 -0800

    theme.css file working

--- keoghs_website/src/index.js
+++ keoghs_website/src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './css/themes.css';
 import './css/index.css';
-import Home from './homePage';
 import reportWebVitals from './other/reportWebVitals';
+
+import AppRouter from './appRouter';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+    <AppRouter/>
   </React.StrictMode>
 );
 

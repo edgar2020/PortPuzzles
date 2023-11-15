59095250ad71c5b8929c51552cdef71fe7aa474e
edgar2020 <evmelendrez@gmail.com>
2023-11-15 14:11:57 -0800

    home option component made (first draft)

--- keoghs_website/src/homePage.js
+++ keoghs_website/src/homePage.js
@@ -1,12 +1,12 @@
 import './css/homePage.css';
 import SiteHeader from './components/header'
+import HomeOptions from './components/homeOptions'
 
 function homePage() {
   return (
     <div className="homePage">
         <SiteHeader/>
-      <h1>Home Page</h1>
-
+        <HomeOptions/>
     </div>
   );
 }

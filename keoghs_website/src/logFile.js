39da6d80b7a5b0e01fc75d5b5d4e301cabda86d1
edgar2020 <evmelendrez@gmail.com>
2023-11-28 00:24:02 -0800

    logFile read now works for current years log file

--- keoghs_website/src/logFile.js
+++ keoghs_website/src/logFile.js
@@ -1,13 +1,29 @@
 
-import React, { useState } from "react";
+import React, { Component, useEffect, useState  } from "react";
 import db from './firebase';
 import {Timestamp} from 'firebase/firestore'
-
+import './css/log.css' 
 
 function getCurrentLogFile()
 {
     let year = new Date().getFullYear();
     return "Keoghs_Port_"+ year;
+}
+
+function getFormatedDate(date)
+{
+    var date = new Date();
+        var year = date.getFullYear();
+        var month = date.getMonth() + 1;
+        var day = date.getDate();
+        var hh = date.getHours();
+        var mm = date.getMinutes();
+        let curDate = ((month < 10 ? "0" + month : month)+'/'+
+                       (day < 10 ? "0" + day : day) +'/'+
+                       (year%1000) + ' '+ 
+                       (hh < 10 ? "0" + hh : hh) + ':'+
+                       (mm < 10 ? "0" + mm : mm));
+        return "["+curDate+"]";
 }
 
 // function createTimestamp()
@@ -33,10 +49,60 @@ function getCurrentLogFile()
 
 export function saveEvent(m)
 {   
-    // let timestamp = createTimestamp();
-
     db.collection(getCurrentLogFile()).add({
-        timestamp:  Timestamp.fromDate(new Date()),
+        time_stamp:  Timestamp.fromDate(new Date()),
         message: m,
     });
 }
+
+
+const LogPage = () =>
+{
+    const [loading, setLoading] = useState(true);
+    const [entries, setEntries] = useState([]);
+ 
+    useEffect(() => {
+        const getEntriesFromFirebase = [];
+        const subscriber = db
+          .collection(getCurrentLogFile())
+          .onSnapshot((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                getEntriesFromFirebase.push({
+                ...doc.data(), //spread operator
+                key: doc.id, // `id` given to us by Firebase
+              });
+            });
+            setEntries(getEntriesFromFirebase);
+            setLoading(false);
+          });
+          console.log(getEntriesFromFirebase);
+        // return cleanup function
+        return () => subscriber();
+      }, [loading]); // empty dependencies array => useEffect only called once
+ 
+
+ 
+    return (
+        <div className="">
+           
+            <div className="">
+                <table>
+                    <tr>
+                        <th>Timestamp</th>
+                        <th>Message</th>
+                    </tr>
+ 
+                    {entries.map((entry) => 
+                    <tr>
+                        {/* {entry.timestamp} */}
+                        {/* <td key={entry.id} className="tg-0lax">{entry.time_stamp}</td> */}
+                        <td key={entry.id} className="tg-0lax">{getFormatedDate(entry.time_stamp.toDate())}: </td>
+                        <td key={entry.id} className="tg-0lax">{entry.message}</td>
+                    </tr> )
+                }
+                </table>
+            </div>
+        </div>
+    );
+}
+export default LogPage

d6714008886b14338cd91aa7a1579fb7c1dc44d5
Adolfo Bugarin <2002theking2002@gmail.com>
2023-12-02 21:03:21 -0800

    ***NOT FINAL*** ***WORK IN PROGRESS***

--- keoghs_website/src/components/task2Weight/balancingSearchAlgorithm.js
+++ keoghs_website/src/components/task2Weight/balancingSearchAlgorithm.js
@@ -1,11 +1,11 @@
 class Node {
 	constructor(ship) {
 		this.state = ship;
-		this.totalCost = 0;
-		this.heuristicCost= 0;
+		this.pathCost = 0; // total path cost (actual cost in minutes to get to current state)
+		this.heuristicCost = 0; // estimated cost in minutes left to reach balance
         // ((old row, old column), (new row, new column))
-		this.lastMove = [[8,0],[8,0]]; // initial crane position
-		this.lastNode = null;
+		this.move = [[8,0],[8,0]]; // initial crane position
+		this.parent = null;
 	}
 }
 
@@ -40,9 +40,9 @@ function balanceSearch(state) { // returns instructions for fastest balance
 	
     // While there are paths being explored
     while (frontier.length > 0) {
-        // Sort the paths in the frontier by cost, with the lowest-cost paths first
+        // Sort the paths in the frontier by total cost (path + heuristic cost), with the lowest-cost paths first
         frontier.sort(function(a, b) {
-            return (a.totalCost + a.heuristicCost) - (b.totalCost + b.heuristicCost);
+            return (a.pathCost + a.heuristicCost) - (b.pathCost + b.heuristicCost);
         });
 
         // Choose the lowest-cost path from the frontier
@@ -50,7 +50,7 @@ function balanceSearch(state) { // returns instructions for fastest balance
 
         // Add this nodeto the explored paths
         explored.push(node);
-        // If this nodereaches the goal, return thenode 
+        // If this node reaches the goal, return the node 
         if (isBalanced(node.state)) {
             return getInstructions(node);
         }
@@ -59,60 +59,81 @@ function balanceSearch(state) { // returns instructions for fastest balance
     }
 
     // If there are no paths left to explore, return null to indicate that the goal cannot be reached
-    return null;
+    console.log("ERROR: No Balance found!")
+    return null; //Should never reach here
 }
 
-function expand(frontier, explored, node) { // not finished
+function expand(frontier, explored, node) { // branching function
     // For every column, check every column
     for (let l = 0; l < 12; l++) {
-        if ((l != node.lastMove[1][1] || node.lastMove[0][1] == node.lastMove[1][1]) && node.state[0][l]) {// if (not moving container just moved OR doing first move) AND column has containers 
+        if ((l != node.move[1][1] || node.move[0] == node.move[1]) && node.state[0][l]) {// if (not moving container just moved OR doing first move) AND column has containers 
             for (let d = 0; d < 12; d++) {
                 if (d != l && !node.state[8][d]) { // if not making redundant move, and coulumn has available spot at top
-                    let newNode = createNewNode(node, l, d);
-        
-
-                    // // Calculate the cost of the next step by adding the step's cost to the node's cost
-                    // let cost = step.cost + node.cost;
+                    var tempNode;
+                    // tempNode = createNewNode(node, l, d);
+                    let tempState = getNewState(node.state, getMove(node.state, l, d));
 
                     // // Check if this step has already been explored
-                    // let isExplored = (explored.find( e => {
-                    //     return e.state == newNode.state
-                    // }))
+                    let isExplored = (explored.find( e => {
+                        return e.state == tempNode.state
+                    }))
 
                     // //avoid repeated nodes during the calculation of neighbors
-                    // let isFrontier = (frontier.find( e => {
-                    //     return e.state == newNode.state
-                    // }))
+                    let isFrontier = (frontier.find( e => {
+                        return e.state == tempNode.state
+                    }))
 
 
                     // // If this step has not been explored
-                    // if (!isExplored && !isFrontier) {
-                    //     // Add the step to the frontier, using the cost and the heuristic function to estimate the total cost to reach the goal
-                    //     frontier.push(newNode);
-                    // }
+                    if (!isExplored && !isFrontier) {
+                        // Add the step to the frontier, using the cost and the heuristic function to estimate the total cost to reach the goal
+                        frontier.push(tempNode);
+                    }
                 }
             }
         }
     }
 }
 
-function createNewNode(node, l, d) { // not finished
+function getMove(state, oldColumn, newColumn) {
     let oldRow = 0;
-    while (node.state[oldRow][l])
-        oldRow++;
+    while (state[oldRow + 1][oldColumn]) // finds top container row in old column
+        oldRow++; // increment if container on top of cell
 
     let newRow = 0;
-    while (node.state[newRow][d]) // change
-        newRow++;
+    while (state[newRow][newColumn]) // finds top empty cell in new column
+        newRow++; // increment if cell has container
+
+    return [[oldRow, oldColumn], [newRow, newColumn]]; // the move is returned
+}
+
+function createNewNode(oldNode, move) {
 
 
-    // newNode = new Node;
-    // newNode.state = newState(node.state, l, d);
-    // newNode.totalCost = node.totalCost + pathCost(node, l, d);
-    // newNode.heuristicCost = heuristicCost(node.state);
-    // newNode.lastMove = []
-    // newNode.lastNode = node;
+    let newNode = new Node(getNewState(oldNode.state, move));
+
+
+
+   // newNode.pathCost = oldNode.pathCost + getPathCost(node.state);
+    //newNode.heuristicCost = getHeuristicCost(node.state);
+    newNode.move = move;
+    // newNode.parent = node;
     // return newNode;
+}
+
+function getNewState(oldState, move) {
+    let newState = oldState;
+    newState[move[0][0]][move[0][1]] = 0; // need to change to empty cell   ***CHANGE***
+    newState[move[1][0]][move[1][1]] = oldState[move[0][0]][move[0][1]]; // container is now in new cell
+    return newState;
+}
+
+function getPathCost() {
+
+}
+ 
+function getHeuristicCost() {
+
 }
 
 function performSIFT(state) { // return instructions for SIFT
@@ -126,9 +147,9 @@ function getInstructions(node) {
 }
 
 function getInstructionsHelper(node, instructions) {
-    if (node.lastNode != null)
-        getInstructionsHelper(node.lastNode, instructions);
+    if (node.parent != null)
+        getInstructionsHelper(node.parent, instructions);
     
-    instructions.push(node.lastMove);
+    instructions.push(node.move);
     return instructions;
 }
--- keoghs_website/src/components/task2Weight/task2Weight.js
+++ keoghs_website/src/components/task2Weight/task2Weight.js
@@ -3,8 +3,8 @@ import '../tasks.css';
 import FileUploader from '../fileUploader';
 import { balance } from './balancingSearchAlgorithm';
 
-// Create States for State Machine Task1_States
-  const Task1_States = {
+// Create States for State Machine Task2_States
+  const Task2_States = {
     INIT: 0,              //where form will be uploaded
     // CONFIRM_FILE: 1,      //where form format will be checked and verifed
     // GRAB_INPUTS: 1,       //where inputs from operator are given
@@ -32,52 +32,52 @@ import { balance } from './balancingSearchAlgorithm';
         [null, null, null, null, null, null, null, null, null, null, null, null]//9th row
       ];
 
-  class task1Loading extends Component {
+  class task2Loading extends Component {
     state = {
       // create starting state
-      current: Task1_States.INIT,
+      current: Task2_States.INIT,
       textFromFile: "null",
       loadedFileName: null
     };
 
     handleFileCallback = (fileData) => {
-      // Update the name in the component's state
-      try {
-        console.log("LOADING"); 
-        this.setState({ textFromFile: fileData.text, loadedFileName: fileData.name });
-        var inputLines = fileData.text.split('\n');
-        for (var i = 0; i < inputLines.length; i++)
-        {
-          
-          var numbersFound = inputLines[i].match(/(\d+)/g);
-          var row_num = parseInt(numbersFound[0]);
-          var col_num = parseInt(numbersFound[1]);
-          var weight = parseInt(numbersFound[2]);
-          var name = inputLines[i].substring(19);
-          if(name !== 'UNUSED')
+        // Update the name in the component's state
+        try {
+          console.log("LOADING"); 
+          this.setState({ textFromFile: fileData.text, loadedFileName: fileData.name });
+          var inputLines = fileData.text.split('\n');
+          for (var i = 0; i < inputLines.length; i++)
           {
-            // alert(numbersFound);
-            // alert(row_num + " " + col_num + " " + weight + " " + name);
-            grid[row_num][col_num] = new Container(name, weight);
+            
+            var numbersFound = inputLines[i].match(/(\d+)/g);
+            var row_num = parseInt(numbersFound[0])-1;
+            var col_num = parseInt(numbersFound[1])-1;
+            var weight = parseInt(numbersFound[2]);
+            // var name = inputLines[i].substring(19);
+            var name = inputLines[i].replace(/^(?:[^,]*,){2}[^,]*,/, ' ').trim();
+            if(name === 'UNUSED')  
+            {
+              grid[row_num][col_num] = {container: null, deadSpace: 0};
+            }
+            else if(name === 'NAN')
+            {
+              grid[row_num][col_num] = {container: null, deadSpace: 1};
+            }
+            else
+            {
+              grid[row_num][col_num] = {container: new Container(name, weight), deadSpace: 0, offload: 0};
+            }
+            this.setState({gridState: grid});
           }
-          if(name === 'NAN')
-          {
-            grid[row_num][col_num] = 'NAN';
-          }
-          else
-          {
-            grid[row_num][col_num] = null;
-          }
+          // alert(inputLines.length);
+          // alert(inputLines);
+  
+          this.transition(Task2_States.COMPUTE_STEPS)
+        } catch (error) {
+          alert("ERROR: File not of correct format" + error);
+          this.transition(Task2_States.INIT);
         }
-        // alert(inputLines.length);
-        // alert(inputLines);
-
-        this.transition(Task1_States.COMPUTE_STEPS)
-      } catch (error) {
-        alert("ERROR: File not of correct format" + error);
-        this.transition(Task1_States.INIT);
       }
-    }
 
     transition(to) {
       this.setState({current: to});
@@ -85,13 +85,13 @@ import { balance } from './balancingSearchAlgorithm';
     // define what is shown at each state
     render() {
       switch(this.state.current) {
-        case Task1_States.COMPUTE_STEPS:
+        case Task2_States.COMPUTE_STEPS:
           return this.renderComputeSteps();
-        case Task1_States.DISPLAY_STEPS:
+        case Task2_States.DISPLAY_STEPS:
           return this.renderShowSteps();
-        case Task1_States.FINISH_PROCEDURE:
+        case Task2_States.FINISH_PROCEDURE:
           return this.renderFinishProcedure();
-        case Task1_States.INIT:
+        case Task2_States.INIT:
         default:
           return this.renderInit();
       }
@@ -108,7 +108,7 @@ import { balance } from './balancingSearchAlgorithm';
     renderComputeSteps() {
       balance(grid);
       return (
-        <button onClick={() => this.transition(Task1_States.INIT)}>
+        <button onClick={() => this.transition(Task2_States.INIT)}>
           No Logic Yet
         </button>
       );
@@ -116,7 +116,7 @@ import { balance } from './balancingSearchAlgorithm';
     // logic for showing steps
     renderShowSteps() {
       return (
-        <button onClick={() => this.transition(Task1_States.INIT)}>
+        <button onClick={() => this.transition(Task2_States.INIT)}>
           No Logic Yet
         </button>
       );
@@ -124,11 +124,11 @@ import { balance } from './balancingSearchAlgorithm';
     // logic for the finish procedure
     renderFinishProcedure() {
       return (
-        <button onClick={() => this.transition(Task1_States.INIT)}>
+        <button onClick={() => this.transition(Task2_States.INIT)}>
           No Logic Yet
         </button>
       );
     }
   }
 
-  export default task1Loading;
+  export default task2Loading;

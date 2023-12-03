bc68f6547286c1079177539bc85a9c89c3b0d254
Adolfo Bugarin <2002theking2002@gmail.com>
2023-12-03 15:43:18 -0800

    ***PUSHED TO DEBUG***

--- keoghs_website/src/components/task2Weight/balancingSearchAlgorithm.js
+++ keoghs_website/src/components/task2Weight/balancingSearchAlgorithm.js
@@ -1,6 +1,6 @@
 class Node {
 	constructor(ship) {
-		this.state = ship;
+		this.state = [...ship]; // should make a copy of "ship" grid array
 		this.pathCost = 0; // total path cost (actual cost in minutes to get to current state)
 		this.heuristicCost = 0; // estimated cost in minutes left to reach balance
         // ((old row, old column), (new row, new column))
@@ -9,31 +9,68 @@ class Node {
 	}
 }
 
+const OLD = 0;
+const NEW = 1;
+const ROW = 0;
+const COLUMN = 1;
+
 let x = 0;
 export function balance(ship) {  // returns instructions to balance, already balanced returns empty instructions
 	console.log("START" + x++);
     console.log(ship);
 
-    if (isBalanced(ship))
+    if (isBalanced(ship)) {
+        console.log("ALREADY BALANCED")
         return [];
-    else if (balancePossible(ship)) 
+    } 
+    else if (balancePossible(ship)) {
+        console.log("BALANCING")
         return balanceSearch(ship);
-    else 
+    } 
+    else {
+        console.log("SIFTING")
         return performSIFT(ship);
+    }
 }
 
-function isBalanced(state) { // returns true if balanced, false if isn't
+function isBalanced(state) { // returns true if balanced, false if isn't (MIGHT BE BETTER TO STORE LEFT AND RIGHT SUMS AS NODE VARIABLES)
+    let leftSum = 0
+    for (let column = 0; column < 6; column++) {
+        let row = 0
+        while (row < 9 && state[row][column].deadSpace == 1)
+            row++
+        
+        while (row < 9 && state[row][column].container !== null) {
+            leftSum += state[row][column].container.weight
+            row++
+        }
+    }
 
+    let rightSum = 0
+    for (let column = 6; column < 12; column++) {
+        let row = 0
+        while (row < 9 && state[row][column].deadSpace == 1)
+            row++
+        
+        while (row < 9 && state[row][column].container !== null) {
+            rightSum += state[row][column].container.weight
+            row++
+        }
+    }
+
+    return (Math.min(leftSum, rightSum) / Math.max(leftSum, rightSum)) >= 0.9 // NEED TO TEST/CHECK
 }
 
 function balancePossible(state) { // returns true if possible to balance, false if impossible
-
+    return true;
 }
 
 function balanceSearch(state) { // returns instructions for fastest balance
     let initialNode = new Node(state);
+    //console.log(initialNode.state)
 	// Create a data structure to store the paths that are being explored
 	let frontier = [initialNode];
+    //console.log("INITIAL frontier: " + [...frontier])
 
 	// Create an empty data structure to store the explored paths
 	let explored = [];
@@ -63,90 +100,147 @@ function balanceSearch(state) { // returns instructions for fastest balance
     return null; //Should never reach here
 }
 
-function expand(frontier, explored, node) { // branching function
+function expand(frontier, explored, node) { // branching function, max 12x11 branches
+    console.log("EXPANDING")
+    //console.log("frontier: " + frontier)
+    //console.log("explored: " + explored)
+   // console.log("node: " + node)
     // For every column, check every column
-    for (let l = 0; l < 12; l++) {
-        if ((l != node.move[1][1] || node.move[0] == node.move[1]) && node.state[0][l]) {// if (not moving container just moved OR doing first move) AND column has containers 
-            for (let d = 0; d < 12; d++) {
-                if (d != l && !node.state[8][d]) { // if not making redundant move, and coulumn has available spot at top
-                    var tempNode;
-                    // tempNode = createNewNode(node, l, d);
-                    let tempState = getNewState(node.state, getMove(node.state, l, d));
+    // o: old column, n: new column
+    for (let o = 0; o < 12; o++) {
+        // if not moving container just moved OR doing first move 
+        // used to also check if column has containers by only checking bottom row, but this would not work if first row is NANs
+        if (o != node.move[NEW][COLUMN] || node.move[OLD] == node.move[NEW]) {       // **NEED TO CHANGE TO CORRECTLY COMPARE ARRAYS**
+            for (let n = 0; n < 12; n++) {
+                if (n != o && node.state[8][n].container === null) { // if not making redundant move, and coulumn has available spot at top
+                    let move = getMove(node.state, o, n);
+                    if (move.length > 0) { // if move is valid (old column must have at least 1 container)
+                        let tempState = getNewState(node.state, move);
+                        
+                        // Check if this state has already been explored
+                        let isExplored = (explored.find( e => {
+                            return e.state == tempState                     // ***NEED TO CHANGE FOR COMPARING ARRAYS TO WORK PROPERLY***
+                        }))
+                        
+                        // avoid repeated nodes during the calculation of neighbors
+                        let isFrontier = (frontier.find( e => {
+                            return e.state == tempState                     // ***NEED TO CHANGE FOR COMPARING ARRAYS TO WORK PROPERLY***      
+                        }))
+                        
+                        
+                        // If this state has not been explored
+                        if (!isExplored && !isFrontier) {
+                            let tempNode = new Node(tempState);
+                            tempNode.pathCost = node.pathCost + getPathCost(node, move);
+                            tempNode.heuristicCost = getHeuristicCost(tempState);
+                            tempNode.move = move;
 
-                    // // Check if this step has already been explored
-                    let isExplored = (explored.find( e => {
-                        return e.state == tempNode.state
-                    }))
-
-                    // //avoid repeated nodes during the calculation of neighbors
-                    let isFrontier = (frontier.find( e => {
-                        return e.state == tempNode.state
-                    }))
-
-
-                    // // If this step has not been explored
-                    if (!isExplored && !isFrontier) {
-                        // Add the step to the frontier, using the cost and the heuristic function to estimate the total cost to reach the goal
-                        frontier.push(tempNode);
+                            // Add the step to the frontier, using the cost and the heuristic function to estimate the total cost to reach the goal
+                            frontier.push(tempNode);
+                        }
                     }
                 }
             }
         }
     }
+    return frontier
 }
 
 function getMove(state, oldColumn, newColumn) {
     let oldRow = 0;
-    while (state[oldRow + 1][oldColumn]) // finds top container row in old column
+    while (oldRow < 9 && state[oldRow][oldColumn].deadSpace == 1)
+        oldRow++
+
+    // ***NEED TO ADD CHECK THAT NO CONTAINERS ARE IN ROW 8 BETWEEN OLD AND NEW COLUMNS***
+    
+    if (oldRow == 9 || state[oldRow][oldColumn].container === null) // returns empty move if no containers in oldColumn
+        return []
+
+    while (oldRow < 8 && state[oldRow + 1][oldColumn].container !== null) // finds top container row in old column
         oldRow++; // increment if container on top of cell
 
     let newRow = 0;
-    while (state[newRow][newColumn]) // finds top empty cell in new column
+    while (newRow < 9 && state[newRow][newColumn].container !== null) // finds top empty cell in new column
         newRow++; // increment if cell has container
 
-    return [[oldRow, oldColumn], [newRow, newColumn]]; // the move is returned
-}
-
-function createNewNode(oldNode, move) {
-
-
-    let newNode = new Node(getNewState(oldNode.state, move));
-
-
-
-   // newNode.pathCost = oldNode.pathCost + getPathCost(node.state);
-    //newNode.heuristicCost = getHeuristicCost(node.state);
-    newNode.move = move;
-    // newNode.parent = node;
-    // return newNode;
+    return [[oldRow, oldColumn], [newRow, newColumn]] // the move is returned                  **NOT SURE IF RETURN WORKS**
 }
 
 function getNewState(oldState, move) {
-    let newState = oldState;
-    newState[move[0][0]][move[0][1]] = 0; // need to change to empty cell   ***CHANGE***
-    newState[move[1][0]][move[1][1]] = oldState[move[0][0]][move[0][1]]; // container is now in new cell
+    var newState = [...oldState];
+    newState[move[OLD][ROW]][move[OLD][COLUMN]] = {container: null, deadSpace: 0}; // replace old location with empty cell
+    newState[move[NEW][ROW]][move[NEW][COLUMN]] = oldState[move[OLD][ROW]][move[OLD][COLUMN]]; // container is now in new cell      ***MIGHT NEED TO CHANGE TO COPY CONTAINER CORRECTLY***
     return newState;
 }
 
-function getPathCost() {
+function getPathCost(node, move) { // NEED TO TEST
+    let cost = 0;
+    
+    // First will add cost to move crane to old container location
+    // add Manhattan Distance to cost
+    cost += Math.abs(node.move[NEW][ROW] - move[OLD][ROW]) // add vertical crane distance to cost
+    cost += Math.abs(node.move[NEW][COLUMN] - move[OLD][COLUMN]) // add horizontal crane distance to cost
+    
+    let maxMoveHeight = Math.max(node.move[NEW][ROW], move[OLD][ROW])
+    let maxObstacleHeight = maxMoveHeight
+    let left = Math.min(node.move[NEW][COLUMN], move[OLD][COLUMN])
+    let right = Math.max(node.move[NEW][COLUMN], move[OLD][COLUMN])
+    
+    for (let column = left + 1; column < right; column++) { // for all columns between old and new locations
+        let row = 0
+        while (row < 9 && node.state[row][column].deadSpace == 1)
+            row++
+        
+        while (row < 9 && node.state[row][column].container !== null)
+            row++
+        
+        if (row > maxObstacleHeight)
+            maxObstacleHeight = row
+    }
+    cost += 2 * (maxObstacleHeight - maxMoveHeight) // what goes up must come down
 
+
+
+    // Next will add actual cost of move
+    // add Manhattan Distance to cost
+    cost += Math.abs(move[NEW][ROW] - move[OLD][ROW]) // add vertical distance to cost
+    cost += Math.abs(move[NEW][COLUMN] - move[OLD][COLUMN]) // add horizontal distance to cost
+
+    maxMoveHeight = Math.max(move[NEW][ROW], move[OLD][ROW])
+    maxObstacleHeight = maxMoveHeight
+    left = Math.min(move[NEW][COLUMN], move[OLD][COLUMN])
+    right = Math.max(move[NEW][COLUMN], move[OLD][COLUMN])
+    for (let column = left + 1; column < right; column++) {
+        let row = 0
+        while (row < 9 && node.state[row][column].deadSpace == 1)
+            row++
+        
+        while (row < 9 && node.state[row][column].container !== null)
+            row++
+        
+        if (row > maxObstacleHeight)
+            maxObstacleHeight = row
+    }
+    cost += 2 * (maxObstacleHeight - maxMoveHeight) // what goes up must come down
+
+    return cost
 }
  
-function getHeuristicCost() {
-
+function getHeuristicCost(state) {
+    return 0
 }
 
 function performSIFT(state) { // return instructions for SIFT
-    //let initialNode = new Node(state);
+    return []
 }
 
 function getInstructions(node) {
-    let instructions = [];
+    var instructions = [];
 	instructions = getInstructionsHelper(node, instructions);
     return instructions;
 }
 
-function getInstructionsHelper(node, instructions) {
+function getInstructionsHelper(node, instructions) { // NEED TO TEST
     if (node.parent != null)
         getInstructionsHelper(node.parent, instructions);
     
--- keoghs_website/src/components/task2Weight/task2Weight.js
+++ keoghs_website/src/components/task2Weight/task2Weight.js
@@ -37,38 +37,42 @@ import { balance } from './balancingSearchAlgorithm';
       // create starting state
       current: Task2_States.INIT,
       textFromFile: "null",
-      loadedFileName: null
+      loadedFileName: null,
+        gridState: []
     };
 
     handleFileCallback = (fileData) => {
         // Update the name in the component's state
         try {
-          console.log("LOADING"); 
           this.setState({ textFromFile: fileData.text, loadedFileName: fileData.name });
           var inputLines = fileData.text.split('\n');
           for (var i = 0; i < inputLines.length; i++)
+        {
+          
+          var numbersFound = inputLines[i].match(/(\d+)/g);
+          var row_num = parseInt(numbersFound[0])-1;
+          var col_num = parseInt(numbersFound[1])-1;
+          var weight = parseInt(numbersFound[2]);
+          var name = inputLines[i].replace(/^(?:[^,]*,){2}[^,]*,/, ' ').trim();
+          console.log(row_num + ' ' + col_num + ' ' + weight + ' ' + name)
+          if(name === 'UNUSED')     
           {
-            
-            var numbersFound = inputLines[i].match(/(\d+)/g);
-            var row_num = parseInt(numbersFound[0])-1;
-            var col_num = parseInt(numbersFound[1])-1;
-            var weight = parseInt(numbersFound[2]);
-            // var name = inputLines[i].substring(19);
-            var name = inputLines[i].replace(/^(?:[^,]*,){2}[^,]*,/, ' ').trim();
-            if(name === 'UNUSED')  
-            {
-              grid[row_num][col_num] = {container: null, deadSpace: 0};
-            }
-            else if(name === 'NAN')
-            {
-              grid[row_num][col_num] = {container: null, deadSpace: 1};
-            }
-            else
-            {
-              grid[row_num][col_num] = {container: new Container(name, weight), deadSpace: 0, offload: 0};
-            }
-            this.setState({gridState: grid});
+            grid[row_num][col_num] = {container: null, deadSpace: 0};
           }
+          else if(name === 'NAN')
+          {
+            grid[row_num][col_num] = {container: null, deadSpace: 1};
+          }
+          else
+          {
+            grid[row_num][col_num] = {container: new Container(name, weight), deadSpace: 0};
+          }
+        }
+          for(var i = 0; i < 12; i++)
+          {
+            grid[8][i] = {container: null, deadSpace: 0};
+          }
+          this.setState({gridState: grid});
           // alert(inputLines.length);
           // alert(inputLines);
   
@@ -106,6 +110,7 @@ import { balance } from './balancingSearchAlgorithm';
     }  
     // #TODO: #3 logic for Computeing the steps (where our search function is going to go) 
     renderComputeSteps() {
+        console.log(grid)
       balance(grid);
       return (
         <button onClick={() => this.transition(Task2_States.INIT)}>

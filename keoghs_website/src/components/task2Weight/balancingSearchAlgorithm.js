0fc269d35c998753d57d4dad9ae165d208fd9004
Adolfo Bugarin <2002theking2002@gmail.com>
2023-12-09 21:02:56 -0800

    Uncommented control instructions

--- keoghs_website/src/components/task2Weight/balancingSearchAlgorithm.js
+++ keoghs_website/src/components/task2Weight/balancingSearchAlgorithm.js
@@ -46,26 +46,26 @@ async function balance(ship) {  // returns instructions to balance, already bala
     //console.log(ship);
     consolePrintState(ship)
 
-    // console.log("HEURISTIC COST: " + getHeuristicCost(ship, [8,0]))
-    // console.log("CHECKING BALANCE...")
+    console.log("HEURISTIC COST: " + getHeuristicCost(ship, [8,0]))
+    console.log("CHECKING BALANCE...")
 
-    // if (isBalanced(ship)) {
-    //     console.log("ALREADY BALANCED")
+    if (isBalanced(ship)) {
+        console.log("ALREADY BALANCED")
 
-    //     let buffer = new Array(4).fill(new Array(24).fill({container: null, deadSpace: false})) // 4x24 array of empty cells
-    //     let state = {ship: ship, buffer: buffer, truck: 0}
+        let buffer = new Array(4).fill(new Array(24).fill({container: null, deadSpace: false})) // 4x24 array of empty cells
+        let state = {ship: ship, buffer: buffer, truck: 0}
 
-    //     // returns empty instructions if already balanced
-    //     return [{cost: 0, state: state, initialPos: {pos: [-1, -1], loc: 1}, finalPos: {pos: [-1, -1], loc: 1}}]
-    // } 
-    // else if (balanceIsPossible(ship)) {
-    //     console.log("UNBALANCED & BALANCE POSSIBLE, BALANCING...")
-    //     return balanceSearch(ship) 
-    // } 
-    // else {
-    //     console.log("UNBALANCED & IMPOSSIBLE TO BALANCE, SIFTING...")
-    //     return performSIFT(ship)
-    // }
+        // returns empty instructions if already balanced
+        return [{cost: 0, state: state, initialPos: {pos: [-1, -1], loc: 1}, finalPos: {pos: [-1, -1], loc: 1}}]
+    } 
+    else if (balanceIsPossible(ship)) {
+        console.log("UNBALANCED & BALANCE POSSIBLE, BALANCING...")
+        return balanceSearch(ship) 
+    } 
+    else {
+        console.log("UNBALANCED & IMPOSSIBLE TO BALANCE, SIFTING...")
+        return performSIFT(ship)
+    }
 
     return performSIFT(ship) // FOR TESTING ONLY
 }
@@ -356,7 +356,7 @@ function getMove(state, oldColumn, newColumn) { // returns empty array if move i
     while (column < newColumn) {
         if (state[8][column].deadSpace == 1 || state[8][column].container !== null) // returns invalid if impossible to move from oldColumn to newColumn
             return []
-            
+
         column++
     }
 

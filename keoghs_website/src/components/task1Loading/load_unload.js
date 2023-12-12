078173c22e7ea7b0cc89cfd09a5a3f04417462ed
Adolfo Bugarin <2002theking2002@gmail.com>
2023-12-12 04:17:20 -0800

    added comments

--- keoghs_website/src/components/task1Loading/load_unload.js
+++ keoghs_website/src/components/task1Loading/load_unload.js
@@ -555,16 +555,16 @@ function getPathCost(node, move) {
 function getHeuristicCost(shipState, toUnload, toLoad ) {
     let H = 0
     
-    H += toLoad.length * 100000
+    H += toLoad.length * 100000 // add to H how many containers are left to load x big constant
 
     toUnload.forEach(container => {
         let row = container[ROW]
         let column = container[COLUMN]
         while (row < 9 && shipState[row][column].container !== null) {
-            H += 100000
+            H += 100000 // add to H how many containers are left to unload + number of containers on top x big constant
             row++
         }
-    });
+    })
     
     return H
 }

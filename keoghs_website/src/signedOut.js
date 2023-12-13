9a96f6828e34f20453ac6ff451bc465afb29679d
edgar2020 <evmelendrez@gmail.com>
2023-12-12 21:38:06 -0800

    move sign out

--- keoghs_website/src/signedOut.js
+++ keoghs_website/src/signedOut.js
@@ -14,11 +14,12 @@ function SignedOut() {
             <h1>Software Signed Out</h1>
             <h2>Happy Holidys!</h2>
             <p>Sign back in: <button id='return' className="signedOutButton" onClick={() => signBackIn()}>Sign-In</button></p>
-            {/* <iframe
+            <iframe
             width="860"
+            title=""
             height="484"
             src="/logFiles"
-            ></iframe>; */}
+            ></iframe>;
 
         </div>
         {/* Code below is from: https://codepen.io/codeconvey/pen/xRzQay

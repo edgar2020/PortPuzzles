c7ca152a2867029ca93e187c9996c0d9b60d1d74
edgar2020 <evmelendrez@gmail.com>
2023-12-12 16:38:36 -0800

    reoved iframe for now

--- keoghs_website/src/signedOut.js
+++ keoghs_website/src/signedOut.js
@@ -8,11 +8,11 @@ function SignedOut() {
             <h1>Software Signed Out</h1>
             <h2>Happy Holidys!</h2>
             <p>Sign back in: <button id='return' className="signedOutButton" /*onClick={}*/>Sign-In</button></p>
-            <iframe
+            {/* <iframe
             width="860"
             height="484"
             src="/logFiles"
-            ></iframe>;
+            ></iframe>; */}
 
         </div>
         {/* Code below is from: https://codepen.io/codeconvey/pen/xRzQay

38036a8b205dccc4b88b3ff3d6beb1da51fc715f
edgar2020 <evmelendrez@gmail.com>
2023-12-14 01:48:38 -0800

    hot fix after accidently deleting main firebase

--- keoghs_website/src/firebase.js
+++ keoghs_website/src/firebase.js
@@ -11,21 +11,21 @@ import 'firebase/compat/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCXLAsebzzSfcoXOapmY7P7VhyyCx9smtY",
-  authDomain: "keoghsport-logfiles.firebaseapp.com",
-  projectId: "keoghsport-logfiles",
-  storageBucket: "keoghsport-logfiles.appspot.com",
-  messagingSenderId: "1046833789454",
-  appId: "1:1046833789454:web:5328f857faa8324a01da5f",
-  measurementId: "G-TDTE7CKWK6"
+  // apiKey: "AIzaSyCXLAsebzzSfcoXOapmY7P7VhyyCx9smtY",
+  // authDomain: "keoghsport-logfiles.firebaseapp.com",
+  // projectId: "keoghsport-logfiles",
+  // storageBucket: "keoghsport-logfiles.appspot.com",
+  // messagingSenderId: "1046833789454",
+  // appId: "1:1046833789454:web:5328f857faa8324a01da5f",
+  // measurementId: "G-TDTE7CKWK6"
 
-  // apiKey: "AIzaSyA3fCoNxry8r-3ADna0Ii3lOxU7wx8S778",
-  // authDomain: "testenviroment-aa870.firebaseapp.com",
-  // projectId: "testenviroment-aa870",
-  // storageBucket: "testenviroment-aa870.appspot.com",
-  // messagingSenderId: "556435674214",
-  // appId: "1:556435674214:web:c23bee7bab1a9c822b89a7",
-  // measurementId: "G-14DVBHHEDE"
+  apiKey: "AIzaSyA3fCoNxry8r-3ADna0Ii3lOxU7wx8S778",
+  authDomain: "testenviroment-aa870.firebaseapp.com",
+  projectId: "testenviroment-aa870",
+  storageBucket: "testenviroment-aa870.appspot.com",
+  messagingSenderId: "556435674214",
+  appId: "1:556435674214:web:c23bee7bab1a9c822b89a7",
+  measurementId: "G-14DVBHHEDE"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
